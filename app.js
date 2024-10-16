2f956cead3d880a9720671c2c510a532b3b810ad
EFerri222 <eferri222@gmail.com>
2024-10-16 15:17:55 -0400

    Add hex to RGB conversion function

--- app.js
+++ app.js
@@ -24,3 +24,17 @@ imageUpload.addEventListener('change', function(event) {
 
     img.src = URL.createObjectURL(file);
 });
+
+// Function to convert hex code to RGB
+function hexToRgb(hex) {
+    hex = hex.replace('#', '');
+    if (hex.length === 3) {
+        hex = hex.split('').map(h => h + h).join('');
+    }
+    const bigint = parseInt(hex, 16);
+    return {
+        r: (bigint >> 16) & 255,
+        g: (bigint >> 8) & 255,
+        b: bigint & 255
+    };
+}
