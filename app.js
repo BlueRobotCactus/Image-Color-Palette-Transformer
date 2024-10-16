f428e751a32773f6a36b6f7255a464b6182955c1
EFerri222 <eferri222@gmail.com>
2024-10-16 15:22:00 -0400

    Add color distance calculation function

--- app.js
+++ app.js
@@ -25,6 +25,15 @@ imageUpload.addEventListener('change', function(event) {
     img.src = URL.createObjectURL(file);
 });
 
+// Function to calculate the distance between two colors
+function colorDistance(c1, c2) {
+    return Math.sqrt(
+        Math.pow(c1.r - c2.r, 2) +
+        Math.pow(c1.g - c2.g, 2) +
+        Math.pow(c1.b - c2.b, 2)
+    );
+}
+
 // Function to convert hex code to RGB
 function hexToRgb(hex) {
     hex = hex.replace('#', '');
